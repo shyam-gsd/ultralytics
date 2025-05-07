f4b58e051d6136bbfeb5b42d917f3b7847a049c2
jshyam33 <jshyam618@gmail.com>
2025-05-07 12:07:10 +0200

    freeze layers

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -244,7 +244,7 @@ class BaseTrainer:
             else []
         )
         always_freeze_names = [".dfl"]  # always freeze these layers
-        freeze_layer_names = [f"model.{x}." for x in freeze_list] + always_freeze_names + [x for x in self.args.freeze if isinstance(x, str)]
+        freeze_layer_names = [f"model.{x}." for x in freeze_list] + always_freeze_names + [x for x in freeze_list if isinstance(x, str)]
         for k, v in self.model.named_parameters():
             # v.register_hook(lambda x: torch.nan_to_num(x))  # NaN to 0 (commented for erratic training results)
             if any(x in k for x in freeze_layer_names):
